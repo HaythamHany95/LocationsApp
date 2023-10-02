cd09a0c91c566588da28dd5841d7eff0996927c3
HaythamHany95 <haythamhany95@gmail.com>
2023-10-02 13:42:18 +0300

    Initial Commit

---
+++ Locations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Locations
+//
+//  Created by Haytham on 02/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
